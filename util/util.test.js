3c7373fcccf63defcd10cec82a803a6449ff9b30
Deepak Kulkarni <deepak4wd@gmail.com>
2018-12-10 11:51:09 +0530

    added testcase for multi function

--- util/util.test.js
+++ util/util.test.js
@@ -1,13 +1,20 @@
 const util = require('./util');
 
-it("should add teo numbers", ()=> {
-    var res = util.add(10, 10);
+it("should add two numbers", ()=> {
+    var res = util.add(10, 5);
     if (res !== 15) {
         
         throw new Error(`expected 15 but got ${res}`);
         
     };
-
-
 });
 
+
+it("should add multi numbers", ()=> {
+    var res = util.multi(10, 10);
+    if (res !== 100) {
+        
+        throw new Error(`expected 100 but got ${res}`);
+        
+    };
+});
